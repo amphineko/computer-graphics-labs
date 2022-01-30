19d4bf48fa26a433cfe393445084cd40312fbd6f
amphineko <sukareki@gmail.com>
2022-01-30 13:34:35 +0000

    fix(lab1): possible memory problem
    Signed-off-by: amphineko <sukareki@gmail.com>

--- cs7gv5_lab1.cpp
+++ cs7gv5_lab1.cpp
@@ -1,6 +1,25 @@
 #include "program.cpp"
 
-vertex_t vertices[] = {
+class CS7GV5Lab1 : public Program {
+public:
+    CS7GV5Lab1() : Program("cs7gv5_lab1.vert", "cs7gv5_lab1.frag") {
+        vertices_ = const_cast<vertex_t *>(program_vertices_);
+        vertices_size_ = sizeof(program_vertices_);
+
+        camera_->SetPosition(-1.75f, 2.0f, 3.0f);
+        camera_->SetRotation(-30.0f, 300.0f);
+    }
+
+protected:
+    void Draw() final {
+        Program::Draw();
+        for (GLint i = 0; i < vertices_size_ / sizeof(vertex_t) / 3; ++i) {
+            glDrawArrays(GL_TRIANGLES, i * 3, 3);
+        }
+    }
+
+private:
+    constexpr static const vertex_t program_vertices_[] = {
     {-1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
     {1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
     {-1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
@@ -21,24 +40,6 @@ vertex_t vertices[] = {
     {1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
     {1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f},
 };
-
-class CS7GV5Lab1 : public Program {
-public:
-    CS7GV5Lab1() : Program("cs7gv5_lab1.vert", "cs7gv5_lab1.frag") {
-        vertices_ = &vertices[0];
-        vertices_size_ = sizeof(vertices);
-
-        camera_->SetPosition(-1.75f, 2.0f, 3.0f);
-        camera_->SetRotation(-30.0f, 300.0f);
-    }
-
-protected:
-    virtual void Draw() {
-        Program::Draw();
-        for (GLint i = 0; i < sizeof(vertices) / sizeof(vertex_t) / 3; ++i) {
-            glDrawArrays(GL_TRIANGLES, i * 3, 3);
-        }
-    }
 };
 
 int main() {
